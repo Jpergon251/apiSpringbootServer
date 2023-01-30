742c4e05ecfeb18d2710b9d8ec79e84b08cf406b
Jpergon251 <Jpergon251@g.educaand.es>
2023-01-30 13:08:11 +0100

    Initial commit

---
+++ src/test/java/es/joseantonioperez/proyectospringjose/ProyectoSpringJoseApplicationTests.java
@@ -0,0 +1,13 @@
+package es.joseantonioperez.proyectospringjose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringJoseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
